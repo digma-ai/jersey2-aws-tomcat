55a74b1145bddd8e9a83bd68844e3d4747b5d3a4
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-07-04 18:11:27 +0300

    aws 1 provider

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ LABEL maintainer="skazaz@digma.ai"
 
 ADD target/helloworld-webapp.war /usr/local/tomcat/webapps/
 ADD hello.war /usr/local/tomcat/webapps/
+ADD aws-sdk-java-2.24.13.jar /usr/local/tomcat/lib/
+ADD opentelemetry-javaagent-1334.jar /usr/local/tomcat/lib/
 
 EXPOSE 8080
 CMD ["catalina.sh", "run"]
--- src/main/java/org/aws/SqsProvider2.java
+++ src/main/java/org/aws/SqsProvider2.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
 
+import java.io.File;
 import java.util.concurrent.CompletableFuture;
 
 public class SqsProvider2 {
@@ -15,11 +16,31 @@ public class SqsProvider2 {
     private final SqsAsyncClient sqsClient;
 
     public SqsProvider2() {
+
+        logs();
+
         sqsClient = SqsAsyncClient
                 .builder()
                 .httpClient(NettyNioAsyncHttpClient.builder().maxConcurrency(10).build())
                 .region(Region.EU_WEST_1)
                 .build();
+    }
+
+    private void logs() {
+        try {
+
+            File file = new File("/usr/local/tomcat/lib/aws-sdk-java-2.24.13.jar");
+            if (file.exists()){
+                System.out.println("aws jar exist "+file.getCanonicalPath());
+            }else{
+                System.out.println("aws jar does NOT exist "+file.getCanonicalPath());
+            }
+
+            System.out.println("SqsAsyncClient class loader is "+SqsAsyncClient.class.getClassLoader());
+
+        }catch (Throwable e){
+            e.printStackTrace();
+        }
     }
 
     public void sqsCall() {
