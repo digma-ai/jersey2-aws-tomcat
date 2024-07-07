eb0f5c466da20eb84a4045052215e49ce2e49b15
Shalom Ben Zvi Kazaz <shalom938@gmail.com>
2024-07-07 16:17:04 +0300

    multiple providers

--- src/main/java/org/aws/ProvidersFactory.java
+++ src/main/java/org/aws/ProvidersFactory.java
@@ -33,8 +33,8 @@ public class ProvidersFactory {
 
             providersClasses.forEach(aClass -> threads.add(new Thread(() -> {
                 try {
-                    System.out.println("creating provider on background "+aClass.getSimpleName());
                     Thread.currentThread().sleep(10);
+                    System.out.println("creating provider on background "+aClass.getSimpleName());
                     providers.add(aClass.getDeclaredConstructor().newInstance());
                 } catch (Exception e) {
                     e.printStackTrace();
